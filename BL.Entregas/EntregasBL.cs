f6e40c48ca86e328e4c4a9b0fff7fdc8cb782dd8
Yanes <Yanes@YANES-PC>
2019-08-23 21:48:44 -0600

    agregando el framework

---
+++ BL.Entregas/EntregasBL.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Entregas
+{
+    public class EntregasBL
+    {
+        public BindingList<Entregas> ListaEntregas { get; set; }
+
+        public EntregasBL()
+        {
+            ListaEntregas = new BindingList<Entregas>();
+
+            var entrega1 = new Entregas();
+            entrega1.Nombre = "Selena Maria Lopez Turcios";
+            entrega1.Direccion = "Barrio El Benque, La Ceiba";
+            entrega1.Id = 222;
+            entrega1.Precio = 1000;
+            entrega1.Codigo = 1902;
+            entrega1.Activo = true;
+
+            ListaEntregas.Add(entrega1);
+
+            var entrega2 = new Entregas();
+            entrega2.Nombre = "Ana Maria Castro";
+            entrega2.Direccion = "Res. Campisa, S.P.S, Cortes";
+            entrega2.Id = 111;
+            entrega2.Precio = 500;
+            entrega2.Codigo = 1002;
+            entrega2.Activo = true;
+
+            ListaEntregas.Add(entrega2);
+
+            var entrega3 = new Entregas();
+            entrega3.Nombre = "Gigi Romero Martinez";
+            entrega3.Direccion = "Barrio Las Palmas, S.P.S, Cortes";
+            entrega3.Id = 333;
+            entrega3.Precio = 1000;
+            entrega3.Codigo = 11023;
+            entrega3.Activo = true;
+
+            ListaEntregas.Add(entrega3);
+
+        }
+        public BindingList<Entregas> ObtenerEntregas()
+        {
+            return ListaEntregas;
+        }
+    }
+    public class Entregas
+    {
+        public string Nombre { get; set; }
+        public int Id { get; set; }
+        public string Direccion { get; set; }
+        public double Precio { get; set; }
+        public int Codigo { get; set; }
+        public bool Activo { get; set; }
+
+    }
+}
---
+++ BL.Entregas/IngresosBL.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Entregas
+{
+    public class IngresosBL
+    {
+        Contexto _contexto;
+        public BindingList<Ingreso> ListaIngresos { get; set; }
+
+        public IngresosBL()
+        {
+
+            _contexto = new Contexto();
+            ListaIngresos = new BindingList<Ingreso>();
+
+           
+
+        }
+        public BindingList<Ingreso> ObtenerIngresos()
+        {
+            _contexto.ingresos.Load();
+            ListaIngresos = _contexto.ingresos.Local.ToBindingList();
+
+            return ListaIngresos;
+        }
+        public Resultado GuardarIngreso(Ingreso ingreso)
+        {
+            var resultado = Validar(ingreso);
+            if (resultado.Exitoso == false)
+            {
+                return resultado;
+            }
+
+            _contexto.SaveChanges();
+            resultado.Exitoso = true;
+            return resultado;
+        }
+        public void AgregarIngreso()
+        {
+            var nuevoIngreso = new Ingreso();
+            ListaIngresos.Add(nuevoIngreso);
+        }
+
+        public bool EliminarIngreso(int id)
+        {
+            foreach (var ingreso in ListaIngresos)
+            {
+                if (ingreso.id == id)
+                {
+                    ListaIngresos.Remove(ingreso);
+                    _contexto.SaveChanges();
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private Resultado Validar(Ingreso ingreso)
+        {
+            var resultado = new Resultado();
+            resultado.Exitoso = true;
+
+
+            if ( String.IsNullOrEmpty (ingreso.Descripcion) == true)
+            {
+                resultado.Mensaje = "Iingrese una descripcion";
+                resultado.Exitoso = false;
+            }
+
+            if (ingreso.Codigo<0)
+            {
+                resultado.Mensaje = "El codigo debe ser mayor que cero";
+                resultado.Exitoso = false;
+            }
+            if (ingreso.Precio < 0)
+            {
+                resultado.Mensaje = "El codigo debe ser mayor que cero";
+                resultado.Exitoso = false;
+            }
+
+
+
+            return resultado;
+        
+            
+          }
+
+
+    }            
+    public class Ingreso
+        {
+            public int id { get; set; }
+            public string Nombre { get; set; }
+            public string Descripcion { get; set; }
+            public double Precio { get; set; }
+            public int Codigo { get; set; }
+            public bool Activo { get; set; }
+            
+        }
+        public class Resultado
+        {
+            public bool Exitoso { get; set; }
+            public string Mensaje { get; set; }
+        }
+    }
+
+
